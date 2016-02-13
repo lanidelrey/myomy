053a4fd0f47a584d4cfc827f36f915b419e17856
Leilani Reyes <leilani3@stanford.edu>
2016-02-13 03:32:15 -0800

    Created test script that output everything

--- firstScript.lua
+++
@@ -1,7 +0,0 @@
-scriptId = 'com.myomy.scripts.leilanitest'
-scriptTitle = "Leilani's Test Script"
-scriptDetailsUrl = ""
-
-function onPoseEdge(pose, edge)
-    myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
-end
